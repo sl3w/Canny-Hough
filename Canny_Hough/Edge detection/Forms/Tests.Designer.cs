0a8b2ad001d0b79a944ce69ff882c54f233a0005
Leo <33494928+sl3w@users.noreply.github.com>
2021-04-13 22:12:40 +0400

    fixing

---
+++ Canny_Hough/Edge detection/Forms/Tests.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Edge_detection.Forms
+{
+    partial class Tests
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(11, 34);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Multiline = false;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(227, 25);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Edge_detection.Properties.Resources.no_photo;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 98);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 176);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 34);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Обзор...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 61);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ошибка при загрузке изображения.";
+            this.label3.Visible = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(380, 34);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox2.Multiline = false;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(227, 25);
+            this.richTextBox2.TabIndex = 12;
+            this.richTextBox2.Text = "";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Edge_detection.Properties.Resources.no_photo;
+            this.pictureBox2.Location = new System.Drawing.Point(380, 98);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(305, 176);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(611, 34);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 24);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Обзор...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(441, 61);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ошибка при загрузке изображения.";
+            this.label1.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(310, 295);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 24);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Обработать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "label2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 381);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "label5";
+            // 
+            // Tests
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 414);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Name = "Tests";
+            this.Text = "Tests";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Canny_Hough/Edge detection/Forms/Tests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Edge_detection.Forms
+{
+    public partial class Tests : Form
+    {
+        public Tests()
+        {
+            InitializeComponent();
+        }
+
+        private Image uploadedImage;
+        private string fileImagePath;
+
+        private Image uploadedImage2;
+        private string fileImagePath2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fileImagePath = LoadAndSaveImage.UploadImage();
+            if (fileImagePath != null)
+                ShowUploadedImage();
+        }
+
+        private void ShowUploadedImage()
+        {
+            try
+            {
+                uploadedImage = Image.FromFile(fileImagePath);
+                if (uploadedImage.Height >= 200 && uploadedImage.Height <= 1500)
+                {
+                    if (uploadedImage.Width >= 200 && uploadedImage.Width <= 2000)
+                    {
+                        pictureBox1.Image = uploadedImage;
+                        button3.Enabled = true;
+                        richTextBox1.Text = fileImagePath;
+                    }
+                    else
+                    {
+                        label3.Text = "Неверные размеры изображения.";
+                        label3.Visible = true;
+                    }
+                }
+                else
+                {
+                    label3.Text = "Неверные размеры изображения.";
+                    label3.Visible = true;
+                }
+            }
+            catch
+            {
+                label3.Text = "Ошибка при загрузке изображения.";
+                label3.Visible = true;
+            }
+        }
+
+        private void ShowUploadedImage2()
+        {
+            try
+            {
+                uploadedImage2 = Image.FromFile(fileImagePath2);
+                if (uploadedImage2.Height >= 200 && uploadedImage2.Height <= 1500)
+                {
+                    if (uploadedImage2.Width >= 200 && uploadedImage2.Width <= 2000)
+                    {
+                        pictureBox2.Image = uploadedImage2;
+                        button3.Enabled = true;
+                        richTextBox2.Text = fileImagePath2;
+                    }
+                    else
+                    {
+                        label1.Text = "Неверные размеры изображения.";
+                        label1.Visible = true;
+                    }
+                }
+                else
+                {
+                    label1.Text = "Неверные размеры изображения.";
+                    label1.Visible = true;
+                }
+            }
+            catch
+            {
+                label1.Text = "Ошибка при загрузке изображения.";
+                label1.Visible = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fileImagePath2 = LoadAndSaveImage.UploadImage();
+            if (fileImagePath2 != null)
+                ShowUploadedImage2();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var Test = new TestsProcessing(new Bitmap(uploadedImage), new Bitmap(uploadedImage2));
+            label2.Text = "PCO:" + Test.CalcPCO();
+            label4.Text = "Pnd:" + Test.CalcPnd();
+            label5.Text = "Pfa:" + Test.CalcPfa();
+        }
+    }
+}
